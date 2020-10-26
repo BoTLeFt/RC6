c386980eb5b195c3e219d6e2989add811b0fc9bc
BoTLeFt <erik_shahov@mail.ru>
2020-10-26 18:21:23 +0300

    Initial commit

---
+++ RC6_1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RC6_1
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.TextBoxCrypted = new System.Windows.Forms.TextBox();
+            this.LabelCryptedText = new System.Windows.Forms.Label();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.LabelKey = new System.Windows.Forms.Label();
+            this.TextBoxKey = new System.Windows.Forms.TextBox();
+            this.LabelOpenText = new System.Windows.Forms.Label();
+            this.TextBoxOpenText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.EncryptButton.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EncryptButton.Location = new System.Drawing.Point(205, 552);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(154, 48);
+            this.EncryptButton.TabIndex = 2;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = false;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // TextBoxCrypted
+            // 
+            this.TextBoxCrypted.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxCrypted.Location = new System.Drawing.Point(33, 397);
+            this.TextBoxCrypted.Multiline = true;
+            this.TextBoxCrypted.Name = "TextBoxCrypted";
+            this.TextBoxCrypted.Size = new System.Drawing.Size(775, 126);
+            this.TextBoxCrypted.TabIndex = 6;
+            // 
+            // LabelCryptedText
+            // 
+            this.LabelCryptedText.AutoSize = true;
+            this.LabelCryptedText.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelCryptedText.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LabelCryptedText.Location = new System.Drawing.Point(33, 368);
+            this.LabelCryptedText.Name = "LabelCryptedText";
+            this.LabelCryptedText.Size = new System.Drawing.Size(142, 26);
+            this.LabelCryptedText.TabIndex = 7;
+            this.LabelCryptedText.Text = "Crypted text";
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DecryptButton.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DecryptButton.Location = new System.Drawing.Point(490, 552);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(154, 48);
+            this.DecryptButton.TabIndex = 8;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = false;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // LabelKey
+            // 
+            this.LabelKey.AutoSize = true;
+            this.LabelKey.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelKey.Location = new System.Drawing.Point(33, 19);
+            this.LabelKey.Name = "LabelKey";
+            this.LabelKey.Size = new System.Drawing.Size(54, 26);
+            this.LabelKey.TabIndex = 4;
+            this.LabelKey.Text = "Key";
+            // 
+            // TextBoxKey
+            // 
+            this.TextBoxKey.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxKey.Location = new System.Drawing.Point(33, 48);
+            this.TextBoxKey.Multiline = true;
+            this.TextBoxKey.Name = "TextBoxKey";
+            this.TextBoxKey.Size = new System.Drawing.Size(775, 126);
+            this.TextBoxKey.TabIndex = 5;
+            this.TextBoxKey.Tag = "";
+            // 
+            // LabelOpenText
+            // 
+            this.LabelOpenText.AutoSize = true;
+            this.LabelOpenText.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelOpenText.Location = new System.Drawing.Point(33, 186);
+            this.LabelOpenText.Name = "LabelOpenText";
+            this.LabelOpenText.Size = new System.Drawing.Size(118, 26);
+            this.LabelOpenText.TabIndex = 9;
+            this.LabelOpenText.Text = "Open Text";
+            // 
+            // TextBoxOpenText
+            // 
+            this.TextBoxOpenText.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxOpenText.Location = new System.Drawing.Point(33, 215);
+            this.TextBoxOpenText.Multiline = true;
+            this.TextBoxOpenText.Name = "TextBoxOpenText";
+            this.TextBoxOpenText.Size = new System.Drawing.Size(775, 126);
+            this.TextBoxOpenText.TabIndex = 10;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(842, 612);
+            this.Controls.Add(this.TextBoxOpenText);
+            this.Controls.Add(this.LabelOpenText);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.LabelCryptedText);
+            this.Controls.Add(this.TextBoxCrypted);
+            this.Controls.Add(this.TextBoxKey);
+            this.Controls.Add(this.LabelKey);
+            this.Controls.Add(this.EncryptButton);
+            this.Name = "MainWindow";
+            this.Text = "RC6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Button EncryptButton;
+        public System.Windows.Forms.TextBox TextBoxCrypted;
+        public System.Windows.Forms.Label LabelCryptedText;
+        public System.Windows.Forms.Button DecryptButton;
+        public System.Windows.Forms.Label LabelKey;
+        public System.Windows.Forms.TextBox TextBoxKey;
+        public System.Windows.Forms.Label LabelOpenText;
+        public System.Windows.Forms.TextBox TextBoxOpenText;
+    }
+}
+
---
+++ RC6_1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RC6_1
+{   
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class RC6
+        {
+            // Константы для операций сдвига
+            public const int W = 32;
+            public const int R = 16;
+
+            // Переменные для файлов
+            public Byte[] File;
+            public uint FileLen;
+
+            // Список итоговых данных
+            public List<Byte> Result = new List<Byte>();
+
+            // Сдвиг вправо
+            public UInt32 ShiftR(UInt32 z_value, int z_shift)
+            {
+                return ((z_value >> z_shift) | (z_value << (W - z_shift)));
+            }
+
+            // Сдвиг влево
+            public UInt32 ShiftL(UInt32 z_value, int z_shift)
+            {
+                return ((z_value << z_shift) | (z_value >> (W - z_shift)));
+            }
+
+            // Определения числа сдвигов
+            public int CountOfShift(int TVar)
+            {
+                int Lg = (int)(Math.Log((double)W) / Math.Log(2.0));
+
+                TVar = TVar << (W - Lg);
+                TVar = TVar >> (W - Lg);
+
+                return TVar;
+            }
+
+            // Генерации ключа
+            public void Generator(UInt32 TKey)
+            {
+                UInt32 P32 = 0xB7E15163;
+                UInt32 Q32 = 0x9E3779B9;
+                UInt32 i, A, B;
+                UInt32 ByteOne, ByteTwo, ByteThree, ByteFour;
+
+                ByteOne = TKey >> 24;
+                ByteTwo = TKey >> 8;
+                ByteTwo = ByteTwo & 0x0010;
+                ByteThree = TKey << 8;
+                ByteThree = ByteThree & 0x0100;
+                ByteFour = TKey << 24;
+
+                TKey = ByteOne | ByteTwo | ByteThree | ByteFour;
+
+                key[0] = P32;
+
+                for (i = 1; i < 2 * R + 4; i++)
+                    key[i] = key[i - 1] + Q32;
+
+                i = A = B = 0;
+
+                int v = 3 * Math.Max(1, 2 * R + 4);
+
+                for (int s = 1; s <= v; s++)
+                {
+                    A = key[i] = ShiftL(key[i] + A + B, CountOfShift(3));
+                    B = TKey = ShiftL(TKey + A + B, CountOfShift((int)(A + B)));
+
+                    i = (i + 1) % (2 * R + 4);
+                }
+            }
+
+            // Пеобразования массива UInt32 к списку байт
+            public void ConvertFromUInt32ToByteArray(UInt32[] array)
+            {
+                List<byte> results = new List<byte>();
+                Result.Clear();
+
+                foreach (UInt32 value in array)
+                {
+                    byte[] converted = BitConverter.GetBytes(value);
+                    results.AddRange(converted);
+                }
+
+                // Формирование списка зашифрованных / расшифрванных байт данных
+                Result.AddRange(results);
+            }
+
+            // Преобразования массива байт в массив UInt32
+            public UInt32[] ConvertFromByteArrayToUIn32(byte[] array, int position)
+            {
+                List<UInt32> results = new List<UInt32>();
+                int length = position + 16;            // Размер блока конвертируемых данных. Читаем по 16 байт.
+
+                for (int i = position; i < length; i += 4)
+                {
+                    byte[] temp = new byte[4];
+
+                    for (int j = 0; j < 4; ++j)
+                    {
+                        if (i + j < array.Length)
+                            temp[j] = array[i + j];
+                        else
+                            temp[j] = 0x00;         // заполняем недостающие блоки в случае достижения конца файла
+                    }
+
+                    results.Add(BitConverter.ToUInt32(temp, 0));
+                }
+
+                return results.ToArray();
+            }
+
+            // Расшифровка
+            public void Decode()
+            {
+                UInt32[] Temp;
+
+                for (int i = 0; i < FileLen; i += 16)
+                {
+                    Temp = ConvertFromByteArrayToUIn32(File, i);
+
+                    Temp[1] = (Temp[1] + key[0]);
+                    Temp[3] = (Temp[3] + key[1]);
+
+                    for (int j = 1; j <= R; j++)
+                    {
+                        UInt32 t = ShiftL((Temp[1] * (2 * Temp[1] + 1)),
+                                            CountOfShift((int)(Math.Log((double)W) / Math.Log(2.0))));
+                        UInt32 u = ShiftL((Temp[3] * (2 * Temp[3] + 1)),
+                                            CountOfShift((int)(Math.Log((double)W) / Math.Log(2.0))));
+
+                        Temp[0] = (ShiftL(Temp[0] ^ t, CountOfShift((int)u)) + key[2 * j]);
+                        Temp[2] = (ShiftL(Temp[2] ^ u, CountOfShift((int)t)) + key[2 * j + 1]);
+
+                        UInt32 temp = Temp[0];
+                        Temp[0] = Temp[1];
+                        Temp[1] = Temp[2];
+                        Temp[2] = Temp[3];
+                        Temp[3] = temp;
+                    }
+
+                    Temp[0] = (Temp[0] + key[2 * R + 2]);
+                    Temp[2] = (Temp[2] + key[2 * R + 3]);
+
+                    ConvertFromUInt32ToByteArray(Temp);                 // Конвертируем в выходной массив расшифрованных данных
+                }
+
+                Temp = null;
+            }
+
+            // Шифрование
+            public void Encode()
+            {
+                UInt32[] TTemp;
+
+                for (int i = 0; i < FileLen; i += 16)
+                {
+                    TTemp = ConvertFromByteArrayToUIn32(File, i);
+
+                    TTemp[0] = (TTemp[0] - key[2 * R + 2]);
+                    TTemp[2] = (TTemp[2] - key[2 * R + 3]);
+
+                    for (int j = R; j >= 1; j--)
+                    {
+                        UInt32 temp = TTemp[3];
+                        TTemp[3] = TTemp[2];
+                        TTemp[2] = TTemp[1];
+                        TTemp[1] = TTemp[0];
+                        TTemp[0] = temp;
+
+                        UInt32 t = ShiftL((TTemp[1] * (2 * TTemp[1] + 1)),
+                                            CountOfShift((int)(Math.Log((double)W) / Math.Log(2.0))));
+                        UInt32 u = ShiftL((TTemp[3] * (2 * TTemp[3] + 1)),
+                                            CountOfShift((int)(Math.Log((double)W) / Math.Log(2.0))));
+
+                        TTemp[0] = (ShiftR((TTemp[0] - key[2 * j]), CountOfShift((int)u))) ^ t;
+                        TTemp[2] = (ShiftR((TTemp[2] - key[2 * j + 1]), CountOfShift((int)t))) ^ u;
+                    }
+
+                    TTemp[1] = (TTemp[1] - key[0]);
+                    TTemp[3] = (TTemp[3] - key[1]);
+
+                    ConvertFromUInt32ToByteArray(TTemp);          // Конвертируем в выходной массив зашифрованных данных
+                }
+
+                TTemp = null;
+            }
+            // Ключ
+            public UInt32[] key = new UInt32[2 * R + 4];
+        }
+        public void EncryptButton_Click(object sender, EventArgs e)
+        {
+            var ob = new RC6();
+            UInt32 key = UInt32.Parse(TextBoxKey.Text);
+            ob.File = System.Text.Encoding.Default.GetBytes(TextBoxOpenText.Text); // Чтение шифруемых данных
+            ob.FileLen = (uint)ob.File.Length;
+            ob.Generator((UInt32)key);        // формирование ключа
+            ob.Encode();                              // шифрование
+            TextBoxCrypted.Text = System.Text.Encoding.Default.GetString(ob.Result.ToArray()); //Вывод на экран зашифрованных данных
+        }
+
+        public void DecryptButton_Click(object sender, EventArgs e)
+        {
+            var ob = new RC6();
+            UInt32 key = UInt32.Parse(TextBoxKey.Text);
+            ob.File = System.Text.Encoding.Default.GetBytes(TextBoxCrypted.Text); //Подготовка данных к расшифрованию
+            ob.FileLen = (uint)ob.File.Length;
+            ob.Generator((UInt32)key);
+            ob.Decode();    // расшифрование
+            TextBoxOpenText.Text = System.Text.Encoding.Default.GetString(ob.Result.ToArray()); //Вывод на экран расшифрованных данных
+        }
+
+
+    }
+}
